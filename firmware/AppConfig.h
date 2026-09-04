6ca4838a4bcfedc0e94d5f9373733f247882b49a
Hamish Twaddle <hamish.twaddle@gmail.com>
2026-09-04 15:48:50 +1200

    Change MOTOR_PWM_PIN and MDNS_HOSTNAME values
    Updated MOTOR_PWM_PIN and MDNS_HOSTNAME for configuration.

--- firmware/AppConfig.h
+++ firmware/AppConfig.h
@@ -3,7 +3,7 @@
 
 namespace AppConfig {
   // ESP32-C3 SuperMini output pin.
-  constexpr uint8_t MOTOR_PWM_PIN = 4;
+  constexpr uint8_t MOTOR_PWM_PIN = 1;
   constexpr uint32_t PWM_FREQUENCY_HZ = 5000;
   constexpr uint8_t PWM_RESOLUTION_BITS = 8;
   constexpr uint16_t PWM_MAX_DUTY = 255;
@@ -17,7 +17,7 @@ namespace AppConfig {
   constexpr char AP_PASSWORD[] = "motor1234"; // At least 8 characters.
 
   // Available as motor.local on networks/devices that support mDNS.
-  constexpr char MDNS_HOSTNAME[] = "motor";
+  constexpr char MDNS_HOSTNAME[] = "HVT_Vibration";
 
   constexpr uint32_t WIFI_CONNECT_TIMEOUT_MS = 15000;
   constexpr uint32_t WIFI_RECONNECT_INTERVAL_MS = 30000;
