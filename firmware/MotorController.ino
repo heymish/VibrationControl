86791e7585ba5fc41701321db8e02c1cc76f20d3
Hamish Twaddle <hamish.twaddle@gmail.com>
2026-09-13 12:26:35 +1200

    Update for ESP32-C5-mini-kit target

--- firmware/MotorController.ino
+++ firmware/MotorController.ino
@@ -12,9 +12,9 @@ void setup() {
   delay(500);
 
   Serial.println();
-  Serial.printf("ESP32-C3 Montor Controller v%s strarting\n",AppConfig::FIRMWARE_VERSION);
+  Serial.printf("ESP32-C5 Motor Controller v%s starting\n", AppConfig::FIRMWARE_VERSION);
   Serial.println();
-  Serial.println("ESP32-C3 motor controller starting");
+  Serial.println("ESP32-C5 motor controller starting");
 
   motor.begin();       // Always starts at 0%.
   network.begin();     // Saved Wi-Fi first, setup AP on failure.
